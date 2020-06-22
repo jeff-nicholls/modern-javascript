adcd6b9d51b34a75054b7843f9a5473491cd20b5
jeff-nicholls <54220470+jeff-nicholls@users.noreply.github.com>
2020-06-21 21:16:56 -0400

    added subfolders

---
+++ part1/app.js
@@ -0,0 +1,22 @@
+// Example 1
+if (1 == 1) {
+    console.log('It\'s True!');
+}
+
+// Example 2 - Performance Review
+// 3 - superstar
+// 2 - meets expectations
+// 1 - needs improvement
+// anything else - wtf?
+let rating = 3;
+
+if (rating === 3) {
+    console.log('YOU ARE A SUPERSTAR!')
+}
+
+// Example 3
+let num = 38;
+
+if (num % 2 !== 0) {
+    console.log('ODD NUMBER!')
+}
