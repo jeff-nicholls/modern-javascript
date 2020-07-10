0133bb25752d159a73f65ea3db392278cc18d5a5
jeff-nicholls <54220470+jeff-nicholls@users.noreply.github.com>
2020-07-09 21:30:30 -0400

    added dropdown

--- movie-fight/index.js
+++ movie-fight/index.js
@@ -6,19 +6,42 @@ const fetchData = async (searchTerm) => {
     },
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+  <label><b>Search for a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown is-active">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`;
 
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  dropdown.classList.add("is-active");
+  for (let movie of movies) {
+    const option = document.createElement("a");
+
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${movie.Poster}" />
+      ${movie.Title}
+    `;
+
+    resultsWrapper.appendChild(option);
   }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
 };
-
-input.addEventListener("input", onInput);
+input.addEventListener("input", debounce(onInput, 500));
